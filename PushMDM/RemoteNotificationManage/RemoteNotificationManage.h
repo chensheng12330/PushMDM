14318d06c46a5b39873b3f0f896c516d3c4b189e
sherwin <sherwin@sherwintekiMac-mini.local>
2013-11-04 09:51:32 +0800

    add all

---
+++ PushMDM/RemoteNotificationManage/RemoteNotificationManage.h
@@ -0,0 +1,83 @@
+//
+//  RemoteNotificationManger.h
+//  Mixc
+//
+//  Created by Knife on 12-11-20.
+//  Update by sherwin.chen on 13-08-23
+//  Copyright (c) 2012年 Knife. All rights reserved.
+//
+
+//版权所有：版权所有(C) 2013，陈胜 [Sherwin.Chen]
+//系统名称：<#使用系统#>
+//文件名称：<#文件名称#>
+//作　　者：陈胜
+//个人联系：chensheng12330@gmail.com or @checkchen2011
+//创建日期：12-11-20
+//修改日期：13-08-23
+//完成日期：<#完成日期#>
+//版   本：版本v0.1.2
+//版本说明：
+/*
+ v0.0.1   基础版本
+ v0.1.2   1、增加产品服务端消息处理能力 2、加入地理地址请求管理
+ */
+//功能说明：对消息推送进行管理
+//---------------------------------------------------------
+
+#import <UIKit/UIKit.h>
+//#import "ASIHTTPRequest.h"
+//#import "LBSLocation.h"
+//#import "PushRegisterDao.h"
+
+@interface RemoteNotificationManage : NSObject
+{
+    //old code
+    //ASIHTTPRequest *_registerRequest;
+    //ASIHTTPRequest *_unRegisterRequest;
+    //end old code
+    
+    //new code
+    
+    NSString *_deviceToken;
+    BOOL isOpen;
+    
+    /*
+    LBSLocation *lbsLocation;
+    CLLocationCoordinate2D coordinate;
+    PushRegisterDao *pushRegisterDao;
+     */
+}
+
+@property (nonatomic, copy) NSString *deviceToken;
+
+// 获得推送管理对象
++ (RemoteNotificationManage*) sharedRemoteNotificationManage;
+
+// 注册推送通知
+- (void) registerNotification;
+
+// 注册失败
+- (void) failToRegisterForRemoteNotificationsWithError:(NSError*)error;
+
+// 注册成功
+- (void) registerForRemoteNotificationsWithDeviceToken:(NSData*)deviceToken;
+
+// 接收到远程通知
+- (void) receiveRemoteNotification:(NSDictionary *)userInfo;
+
+// 判断当前是否接收远程通知
+- (BOOL) isOpenNotifications;
+
+// 判断当前系统设置中是否支持推送
+- (BOOL) sysIsOpenRemoteNotification;
+
+// 设置是否接收推送通知
+
+
+@end
+
+#ifndef PUSHMANGER
+
+#define PUSHMANGER ([RemoteNotificationManage sharedRemoteNotificationManage])
+
+#endif
---
+++ PushMDM/SHAppDelegate.m
@@ -0,0 +1,115 @@
+//
+//  SHAppDelegate.m
+//  WebUserAgent
+//
+//  Created by sherwin.chen on 13-9-22.
+//  Copyright (c) 2013年 sherwin.chen. All rights reserved.
+//
+
+#import "SHAppDelegate.h"
+#import "SHViewController.h"
+#import "RemoteNotificationManage.h"
+
+@implementation SHAppDelegate
+
+- (void)dealloc
+{
+    [_window release];
+    [_viewController release];
+    [super dealloc];
+}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    //推送消息启动
+    if (launchOptions) { //如无消息启动模式，则launchOptions为nil
+        
+        NSDictionary* pushNotificationKey = [launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey]; //远程推送
+        if (pushNotificationKey) {
+            [PUSHMANGER receiveRemoteNotification:pushNotificationKey];
+        }
+        else if ([launchOptions objectForKey:UIApplicationLaunchOptionsLocalNotificationKey]) ///本地消息
+        {
+            
+        }
+    }
+    
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0) {
+        self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame]] autorelease];
+        [application setStatusBarStyle:UIStatusBarStyleLightContent];
+    }
+    else {
+        self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
+    }
+    //
+    //self.window.backgroundColor = [UIColor whiteColor];
+    
+    // Override point for customization after application launch.
+    self.viewController = [[[SHViewController alloc] initWithNibName:@"SHViewController" bundle:nil] autorelease];
+    self.window.rootViewController = self.viewController;
+    [self.window makeKeyAndVisible];
+    
+    [PUSHMANGER registerNotification];
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+#pragma mark -
+#pragma mark handle Push
+
+- (void)application:(UIApplication *)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)devToken
+{
+    //注册
+    [PUSHMANGER registerForRemoteNotificationsWithDeviceToken:devToken];
+    return;
+}
+
+//远程通知
+-(void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    
+    
+    [PUSHMANGER receiveRemoteNotification:userInfo];
+    return;
+    
+    /*
+     aps =     {
+     alert = test2;
+     badge = 1;
+     sound = default;
+     };
+     */
+}
+
+//本地通知
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
+{
+    
+}
+@end
